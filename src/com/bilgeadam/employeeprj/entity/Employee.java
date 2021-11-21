5cc82bd8725f0b5c1fa643f9e607396b2f0e0efc
vctorhugotr <emre13679@gmail.com>
2021-11-21 14:21:43 +0300

    Initial commit

---
+++ src/com/bilgeadam/employeeprj/entity/Employee.java
@@ -0,0 +1,54 @@
+package com.bilgeadam.employeeprj.entity;
+
+public class Employee {
+    private String name;
+    private int age;
+    private String designation;
+    private double salary;
+   
+
+    public Employee() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", designation='" + designation + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
---
+++ src/com/bilgeadam/employeeprj/test/EmployeeTest.java
@@ -0,0 +1,19 @@
+package com.bilgeadam.employeeprj.test;
+
+import com.bilgeadam.employeeprj.entity.Employee;
+
+public class EmployeeTest {
+    public static void main(String[] args) {
+
+
+        Employee emp1 =new Employee()
+        Employee emp2 =new Employee()
+
+                emp1.setAge(70);
+                emp1.setDesignation("DNA Bilimi");
+                emp1.setSalary(20000);
+
+
+
+    }
+}
